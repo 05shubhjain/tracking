a343c532b5e4f99b37ed618b350695e6d4b24c25
05shubhjain <jainshubh853@example.com>
2025-08-19 23:25:19 +0530

    Initial commit

---
+++ custom-functions/id-uxp0x/function-code.dart
@@ -0,0 +1,10 @@
+
+  // return "morning" if it is morning, "afternoon" if afternoon and "night" if it is night
+  var hour = timestamp.hour;
+  if (hour >= 0 && hour < 12) {
+    return "Good morning,";
+  } else if (hour >= 12 && hour < 17) {
+    return "Good afternoon,";
+  } else {
+    return "Goodnight,";
+  }
